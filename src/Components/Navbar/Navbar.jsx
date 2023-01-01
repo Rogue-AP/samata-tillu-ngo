249776f4e6f28620399d677b31444b59b21b2017
RogueAP <teknoofreak@gmail.com>
2023-01-01 15:19:46 -0500

    fixed home nav link

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar() {
     return (
         <header>
             <ul className='navbar'>
-                <li className='navbar-item'><a href="#home">Home</a></li>
+                <li className='navbar-item'><a href="/">Home</a></li>
                 <li className='navbar-item'><a href="#news">News</a></li>
                 <li className='navbar-item'><a href="#contact">Contact</a></li>
                 <li className='navbar-item'><a href="#about">About</a></li>
